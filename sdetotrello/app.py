bbc5ff286de588eb415b8c9e9a202f35b4f66901
Jesse Nestler <jesse.nestler@colorado.edu>
2019-10-11 09:18:19 -0600

    Adds configs to variables in app.py

--- sdetotrello/app.py
+++ sdetotrello/app.py
@@ -1,3 +1,18 @@
 # TODO: add imports and globals envs
+import json
+from .utils import management as mgmt
+
+# Extract configurations
+with open("config.json", 'r') as config_file:
+    configs = json.load(config_file)
+
+# Create a list of all database connections to comb through
+connections = []
+for val in configs["database_connections"].values():
+    connections += val
+# Create a list of keywords that the feature class must contain
+filters = configs["filters"]
+
+
 def main():
     pass
