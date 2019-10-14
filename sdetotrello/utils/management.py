c8256f0f742667bbe0aab4969e2460d8d8fb8f9f
Jesse Nestler <jesse.nestler@colorado.edu>
2019-10-13 22:01:54 -0600

    Adds function to extract trello labels and save to json file.

--- sdetotrello/utils/management.py
+++ sdetotrello/utils/management.py
@@ -1,4 +1,6 @@
 import os
+import json
+from requests import request
 from arcpy.da import Walk
 
 from .features import TrelloFeatureClass
@@ -39,3 +41,31 @@ def find_in_database(database_connections: list = None, *args) -> tuple:
         filtered_items = list(filter(lambda x: any(arg.lower() in "".join(x).lower() for arg in args), items))
         for item in filtered_items:
             yield TrelloFeatureClass(item)
+
+
+def extract_trello_labels(board: str, key: str, token: str, save_to_file: bool = False) -> dict:
+    """Extracts labels from a trello board and saves them locally as a dictionary
+
+    :param board: the id of the board
+    :param key: user's API key
+    :param token: user's API token
+    :param save_to_file: Whether to save to json file. If yes, saves to ./sdetotrello/trello_labels.json
+    :return:
+    """
+    url = "https://api.trello.com/1/boards/{}/labels".format(board)
+    query = {"fields": ["id", "name", "color"],
+             "key": key,
+             "token": token}
+    response = request("GET", url, params=query)
+    json_data = json.loads(response.text)
+
+    labels = dict()
+    for resp in json_data:
+        if resp["name"] != "":
+            labels[resp["id"]] = {"name": resp["name"], "color": resp["color"]}
+
+    if save_to_file:
+        with open("./sdetotrello/trello_labels.json", "w") as json_file:
+            json.dump(labels, json_file)
+    else:
+        return labels
