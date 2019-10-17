9fb8d674ba16593580c126721099169f4fb1eb26
= <nestlerj@bouldercolorado.gov>
2019-10-17 16:40:00 -0600

    Adds TrelloCard subclass, modifies __init__ of TrelloFeatureClass, changes record_count() to SQL based query

--- sdetotrello/utils/features.py
+++ sdetotrello/utils/features.py
@@ -1,4 +1,4 @@
-from arcpy import Describe, ExecuteError, GetCount_management
+from arcpy import Describe, ExecuteError, ArcSDESQLExecute
 from os import path
 
 
@@ -8,24 +8,30 @@ class TrelloFeatureClass:
     def __init__(self, tuple_path):
         self.tuple_path = tuple_path
         self.full_path = path.join(*self.tuple_path)
-        self._desc_workspace = Describe(self.tuple_path[0])
-        self.owner, self.name = self.tuple_path[-1].split(".")
-        self.dataset = self.tuple_path[1] if len(self.tuple_path) == 3 else None
+        self.connection = tuple_path[0]
+        self._desc_workspace = Describe(self.connection)
         self.database = self._desc_workspace.connectionProperties.instance.split(":")[-1].upper()
+        self.dataset = self.tuple_path[1] if len(self.tuple_path) == 3 else None
+        self.owner, self.name = self.tuple_path[-1].split(".")
         self.unique_name = ".".join([self.database, self.owner, self.name]).upper()
+        self.num_records = self.record_count()
 
     def __getattr__(self, attr):
         """Pass any other attribute or method calls through to the underlying Describe object"""
         return getattr(self._desc_workspace, attr)
 
+    def record_count(self):
+        try:
+            query = """SELECT COUNT(*) FROM {}""".format(self.tuple_path[-1].upper())
+            execute_object = ArcSDESQLExecute(self.connection)
+            result = execute_object.execute(query)
+            return int(result)
+        except (ExecuteError, TypeError):
+            return None
+
     def has_no_records(self):
         """Determines if there are any records in the feature class to analyze."""
-        try:
-            count = int(GetCount_management(self.full_path).getOutput(0))
-            return True if count == 0 else False
-        except ExecuteError:
-            # TODO: Add info logging describing an error in retrieving a feature count
-            return None
+        return True if self.num_records == 0 else False
 
     def geometry_type(self):
         """Describes the feature class rather than the workspace and return the shape type"""
@@ -37,3 +43,10 @@ class TrelloFeatureClass:
                 return None
         except OSError:
             return None
+
+
+class TrelloCard(TrelloFeatureClass):
+    def __init__(self, obj, service_dict, ez_dict):
+        super().__init__(obj.tuple_path)
+        self.services_list = service_dict[self.unique_name]if self.unique_name in service_dict else None
+        self.ez_list = ez_dict[self.unique_name] if self.unique_name in ez_dict else None
